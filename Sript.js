2392e7e4c4f6511ecf1b6eaa5b70c58a91cd6f25
harshavardhan8118 <64030338+harshavardhan8118@users.noreply.github.com>
2021-11-27 01:00:41 +0530

    Update Sript.js

--- Sript.js
+++ Sript.js
@@ -1,7 +1,13 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 request.send();
 request.onload = function()
 {
     console.log(this.response);
+    var data = JSON.parse(this.response);
+    
+    for (i=0;i<data.length;i++)
+    {
+        console.log(data[i].id)
+    }
 }
